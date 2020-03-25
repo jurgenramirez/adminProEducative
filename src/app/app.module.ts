e1abc367ecc4119834195264ed2eff0fa6546ff9
Jurgen <ramirezjurgen@gmail.com>
2020-03-25 15:20:17 -0600

    Maquetacion de Proyecto, Paginas compartidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './share/header/header.component';
+import { NopagefoundComponent } from './share/nopagefound/nopagefound.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    NopagefoundComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
